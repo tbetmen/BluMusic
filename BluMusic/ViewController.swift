a3597b38304a5977f2e1e8ba50e8b6cf785b249c
Muhammad M. Munir <muhammadmmunir24@gmail.com>
2024-05-18 14:38:41 +0700

    Initial Commit

---
+++ BluMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluMusic
+//
+//  Created by Muhammad M. Munir on 18/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
